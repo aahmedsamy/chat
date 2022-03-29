3cd3c5ca6d2d1042011491db65d1debd8dafa6de
ahmedsamy <aahmedsamy.as@gmail.com>
2022-03-29 09:27:45 +0200

    styling web pages, added login required

--- chat/settings.py
+++ chat/settings.py
@@ -35,11 +35,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # third parties
     'channels',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'crispy_forms',
     # my apps
     'chatrooms'
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -128,7 +135,23 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+# all auth
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = 'index'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'index'
+ACCOUNT_EMAIL_REQUIRED = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 
 
-class Index(View):
+class Index(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'chatrooms/index.html')
 
 
-class Room(View):
+class Room(LoginRequiredMixin, View):
     def get(self, request, room_name):
         return render(request, 'chatrooms/room.html', {'room_name': room_name})
